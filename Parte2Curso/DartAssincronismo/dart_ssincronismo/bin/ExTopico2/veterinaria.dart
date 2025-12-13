0ec0e34c2efc2d698f6dedbbade6ca04c042015c
MatheusMachadoRocha <matheusmachadoroc@gmail.com>
2025-12-13 12:27:37 -0300

    nomeQualquer

--- Parte2Curso/DartAssincronismo/dart_ssincronismo/bin/ExTopico2/veterinaria.dart
+++ Parte2Curso/DartAssincronismo/dart_ssincronismo/bin/ExTopico2/veterinaria.dart
@@ -5,10 +5,23 @@ void main(){
 
 }
 
-retornaCalendario() async{
-  String url = "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/vet.json";
+fetchVetAndSchedule(String veterinarian) async {
+  String url =
+      "https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/vet.json";
   Response response = await get(Uri.parse(url));
 
-  List<dynamic> listaConsultas = json.decode(response.body);
-  
+  List<dynamic> listAppointments = json.decode(response.body);
+
+  List<dynamic> vetAppointments = listAppointments
+      .where((appointment) => appointment["veterinarian"] == veterinarian)
+      .toList();
+
+  vetAppointments.sort((a, b) => DateTime.parse(a["appointment"])
+      .compareTo(DateTime.parse(b["appointment"])));
+
+  print("Consultas Agendadas para $veterinarian:");
+  for (Map<String, dynamic> appointment in vetAppointments) {
+    print(
+        "Pet: ${appointment["pet_name"]} - Horário: ${appointment["appointment"]}");
+  }
 }
--- Parte2Curso/DartAssincronismo/dart_ssincronismo/bin/dart_ssincronismo.dart
+++ Parte2Curso/DartAssincronismo/dart_ssincronismo/bin/dart_ssincronismo.dart
@@ -1,11 +1,19 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 
-void main(){
-  print("Ola mundo");
-  requestData();
-}
+void main() {
+  // print("Olá, mundo!");
+  // requestData();
+  // requestDataAsync();
 
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
+}
+ 
 requestData(){
   String url = "https://gist.githubusercontent.com/MatheusMachadoRocha/579418badb8f431c6bea57965603e4a9/raw/cc1651ededcf0929e845f96134c73090e75da4f4/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url)); // usado para pegar um valor do futuro(conhecido como operacao assincrona) o future pega um valor do futuro pois a requisicao demora o response é a resposta gerada que é a url convertida pra uri para pegar a resposta
@@ -19,9 +27,17 @@ requestData(){
    print("Última coisa a acontecer na função."); // nao é a ultima coisa aparecer pois a funcao nao e assincrona a de cima espera entao a debaixo ja roda e dps quando tem a resposta printa o resultado 
 }
 
-requestDataAsync() async { // usa o async para usar o await e dizer que a funcao e assincrona O AWAIT FALA  
+Future<List<dynamic>> requestDataAsync() async { // usa o async para usar o await e dizer que a funcao e assincrona O AWAIT FALA  
     String url = "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
     Response response = await get(Uri.parse(url)); //await espera ate que aconteca para dai proseguir com o andamento padrao
-    print(json.decode(response.body)[0]);
-    print("De fato, a última coisa a acontecer na função.");
+    return json.decode(response.body)[0];
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  print(content);
+  String url = "https://api.github.com/gists/413c0aefe6c6abc464581c29029c8ace";
+  Response response = await post(Uri.parse(url), body: content);
 }
