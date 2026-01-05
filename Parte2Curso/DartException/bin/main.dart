de3ed929edc734dcb3978c9e77747abd5ede3535
MatheusMachadoRocha <matheusmachadoroc@gmail.com>
2026-01-04 21:35:08 -0300

    ajustes finais

--- Parte2Curso/DartException/bin/main.dart
+++ Parte2Curso/DartException/bin/main.dart
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 import '../lib/screens/account_screen.dart';
-=======
-import 'package:dart_exceptions/screens/account_screen.dart';
->>>>>>> a45a5260260bb4e7c53854b2ca7f4220c9f2a968
 
 void main() {
   AccountScreen accountScreen = AccountScreen();
