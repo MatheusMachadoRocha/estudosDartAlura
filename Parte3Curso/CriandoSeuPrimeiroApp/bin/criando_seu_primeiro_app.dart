88158a381aa9626117bc0d1659bc7331e328cb98
MatheusMachadoRocha <matheusmachadoroc@gmail.com>
2026-01-15 12:35:41 -0300

    deletado

--- Parte3Curso/CriandoSeuPrimeiroApp/bin/criando_seu_primeiro_app.dart
+++
@@ -1,5 +0,0 @@
-import 'package:criando_seu_primeiro_app/criando_seu_primeiro_app.dart' as criando_seu_primeiro_app;
-
-void main(List<String> arguments) {
-  print('Hello world: ${criando_seu_primeiro_app.calculate()}!');
-}
--- Parte3Curso/CriandoSeuPrimeiroApp/test/criando_seu_primeiro_app_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:criando_seu_primeiro_app/criando_seu_primeiro_app.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
