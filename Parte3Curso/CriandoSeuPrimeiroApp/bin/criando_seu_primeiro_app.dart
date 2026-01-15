c2b7fa671578e24927724cc6dda17d991eaa28e0
MatheusMachadoRocha <matheusmachadoroc@gmail.com>
2026-01-15 12:31:35 -0300

    principio funcionando para agora ter tela

---
+++ Parte3Curso/CriandoSeuPrimeiroApp/bin/criando_seu_primeiro_app.dart
@@ -0,0 +1,5 @@
+import 'package:criando_seu_primeiro_app/criando_seu_primeiro_app.dart' as criando_seu_primeiro_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${criando_seu_primeiro_app.calculate()}!');
+}
---
+++ Parte3Curso/CriandoSeuPrimeiroApp/test/criando_seu_primeiro_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:criando_seu_primeiro_app/criando_seu_primeiro_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
